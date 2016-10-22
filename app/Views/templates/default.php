f6a9682c7956eaad61fc392f7b4b31b814b8e483
raoul20 <uwiadjakly@yahoo.fr>
2016-10-22 10:12:40 +0200

    Modification des liens vers les fichiers css et js, l'url pointe désormais vers tourrnoi/public et non tournoi/

--- app/Views/templates/default.php
+++ app/Views/templates/default.php
@@ -10,8 +10,8 @@
     <title><?= App::getInstance()->title; ?></title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="/public/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/public/css/login.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/login.css">
     <![endif]-->
 </head>
 
@@ -63,8 +63,8 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script>window.jQuery || document.write('<script src="/public/js/jquery-3.1.1.min.js"><\/script>')</script>
-<script src="/public/js/bootstrap.min.js"></script>
+<script>window.jQuery || document.write('<script src="/js/jquery-3.1.1.min.js"><\/script>')</script>
+<script src="/js/bootstrap.min.js"></script>
 
 </body>
 </html>
