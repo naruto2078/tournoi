57b07a764cbaddf954f9d850c9e9a863684912b6
ismabak <ismabak@hotmail.fr>
2016-11-05 01:20:29 +0100

    ajout de allbyOrganizer

---
+++ app/Entity/TournoiEntity.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Raoul
+ * Date: 19/10/2016
+ * Time: 12:21
+ */
+
+namespace App\Entity;
+
+
+use Core\Entity\Entity;
+
+class TournoiEntity extends Entity {
+ 
+
+   
+
+}
